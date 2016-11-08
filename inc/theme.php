a65d971fa78d6ab066a45c987547659f059faf0d
Austin Crane <austincrane@gmail.com>
2016-11-08 15:09:12 -0500

    button move

--- inc/theme.php
+++ inc/theme.php
@@ -14,9 +14,9 @@ function my_login_logo() { ?>
 <style type="text/css">
   body.login div#login h1 a {
   	background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
-  	background-size: 327px 67px;
-  	width: 327px;
-  	height: 67px;
+  	background-size: 300px 299px;
+  	width: 300px;
+  	height: 299px;
   }
 </style>
 <?php }
